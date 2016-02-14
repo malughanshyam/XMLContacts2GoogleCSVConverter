d24b1fb7d3993caef1d5220e3ba5e92101e4edd0
Ghanshyam Malu <malughanshyam@gmail.com>
2016-02-14 18:23:42 -0500

    Parsing the Structured name fields, nick name, birthday from epoch

--- ContactsXML2CSV.py
+++ ContactsXML2CSV.py
@@ -9,7 +9,7 @@
 	@Version  	: Uses Python 2.7
 '''
 
-import sys,csv,os
+import sys,csv,os,time,traceback
 import xml.etree.ElementTree as ET
 
 def usage():
@@ -25,38 +25,127 @@ def parseXML(fileName):
 	try:
 		tree = ET.parse(fileName)
 		root = tree.getroot()
-		print root
 		
+		# Get all contact nodes
+		allParsedContacts = []
+
+		for contact in root.findall("contact"):		
+			
+			
+			
+			#structuredName Tag
+			displayName = contact.findtext('structuredName/displayName',default='')
+			givenName = contact.findtext('structuredName/givenName', default='')
+			familyName = contact.findtext('structuredName/familyName', default='')
+			prefixName = contact.findtext('structuredName/prefixName', default='')
+			suffixName = contact.findtext('structuredName/suffixName', default='')
+			middleName = contact.findtext('structuredName/middleName', default='')
+			
+			# Nickname
+			nickName = contact.findtext('nickName', default='')
+
+			#Birthday
+			birthday = ''
+			events = contact.find('Events')
+			allEvents = list(events.iter("event"))
+			for event in allEvents:
+				if event.findtext('type') == 'birthday' and event.findtext('start_date') != None:
+					birthdayFromEpoch = float(event.findtext('start_date'))
+					birthday = time.strftime('%Y-%m-%d',  time.gmtime(birthdayFromEpoch/1000.))
+			
+			# Create a new Dictionary
+			newContactDict = {}
+			newContactDict['Name']=displayName
+			newContactDict['Given Name'] = givenName
+			newContactDict['Family Name'] = familyName
+			newContactDict['Name Prefix'] = prefixName
+			newContactDict['Name Suffix'] = suffixName
+			newContactDict['Initials'] = middleName
+			newContactDict['Birthday'] = birthday
+			newContactDict['Nickname'] = nickName
+			
+			
+			print newContactDict,"\n"
+			
+			'''
+			newContactDict['Name']='Puneet Loya'
+			newContactDict['Given Name'] = 'Puneet'
+			newContactDict['Family Name'] = 'Loya'
+			newContactDict['Birthday'] = '1990-06-03'
+			newContactDict['E-mail 1 - Type'] = '* Other'
+			newContactDict['E-mail 1 - Value'] = 'loya.puneet@gmail.com'
+			newContactDict['E-mail 2 - Type'] = 'Other'
+			newContactDict['E-mail 2 - Value'] = 'puneetloya123@yahoo.co.in ::: loyapuneet@gmail.com'
+			newContactDict['Phone 1 - Type'] = 'Home'
+			newContactDict['Phone 1 - Value'] = '+1 812-391-9840'
+			newContactDict['Phone 2 - Type'] = 'Mobile'
+			newContactDict['Phone 2 - Value'] = '+91 81 47 834462'
+			newContactDict['Phone 3 - Type'] = 'Other'	
+			newContactDict['Phone 3 - Value'] = '+91 96 20 309099'	
+			'''
+			
+			
 		
-	except:
-		print "Error parsing the file", fileName
-		sys.exit()
+	except :
+		print "Error parsing the file", fileName, ":\n", sys.exc_info()[0],"\n", sys.exc_info()[1]
+		print sys.exc_info()
+		# traceback.print_stack()
+		traceback.print_exc(file=sys.stdout)
+		#sys.exit()
 	else:
 		return root
 	
-	
+
 def exportCSV(fileName):
+	'''
+	Export to CSV
+	'''
+	global GoogleCSVFieldNames
 	newFileName = os.path.splitext(fileName)[0]+'.csv'
 	print newFileName
-	
-	with open(newFileName, 'w') as csvfile:
-		fieldnames = ['first_name', 'hell','last_name']
-		fieldnames = ('first_name', 'hell','last_name')
-		fieldnames.append("fmasdf")
-		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	try:
+		with open(newFileName, 'w') as csvfile:
 
-		writer.writeheader()
-		writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-		writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-		writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
-	
+			writer = csv.DictWriter(csvfile, fieldnames=GoogleCSVFieldNames, lineterminator='\n')
+			writer.writeheader()
+			newContactDict = {}
+			newContactDict['Name']='Puneet Loya'
+			newContactDict['Given Name'] = 'Puneet'
+			newContactDict['Family Name'] = 'Loya'
+			newContactDict['Birthday'] = '1990-06-03'
+			newContactDict['E-mail 1 - Type'] = '* Other'
+			newContactDict['E-mail 1 - Value'] = 'loya.puneet@gmail.com'
+			newContactDict['E-mail 2 - Type'] = 'Other'
+			newContactDict['E-mail 2 - Value'] = 'puneetloya123@yahoo.co.in ::: loyapuneet@gmail.com'
+			newContactDict['Phone 1 - Type'] = 'Home'
+			newContactDict['Phone 1 - Value'] = '+1 812-391-9840'
+			newContactDict['Phone 2 - Type'] = 'Mobile'
+			newContactDict['Phone 2 - Value'] = '+91 81 47 834462'
+			newContactDict['Phone 3 - Type'] = 'Other'	
+			newContactDict['Phone 3 - Value'] = '+91 96 20 309099'	
+			writer.writerow(newContactDict)
+			
+	except:
+		print "Error exporting CSV:", ":\n", sys.exc_info()[0],"\n", sys.exc_info()[1]
 
 def main():
-# display some lines
 	if len(sys.argv) != 2:
 		usage()
+	
+	#Global
+	global GoogleCSVFieldNames
+	GoogleCSVFieldNames = ['Name','Given Name','Family Name','Name Prefix','Name Suffix','Initials','Nickname','Birthday','Notes','E-mail 1 - Type','E-mail 1 - Value','E-mail 2 - Type','E-mail 2 - Value','Phone 1 - Type','Phone 1 - Value','Phone 2 - Type','Phone 2 - Value','Phone 3 - Type','Phone 3 - Value','Website 1 - Type','Website 1 - Value']
 		
 	parseXML(sys.argv[1])
-	exportCSV(sys.argv[1])
+	#exportCSV(sys.argv[1])
 
-if __name__ == "__main__": main()
+if __name__ == "__main__": 
+	main()
+
+
+'''
+Name,Given Name,Family Name,Name Prefix,Name Suffix,Initials,Nickname,Birthday,Notes,E-mail 1 - Type,E-mail 1 - Value,E-mail 2 - Type,E-mail 2 - Value,Phone 1 - Type,Phone 1 - Value,Phone 2 - Type,Phone 2 - Value,Website 1 - Type,Website 1 - Value
+
+
+'Name','Given Name','Family Name','Name Prefix','Name Suffix','Initials','Nickname','Birthday','Notes','E-mail 1 - Type','E-mail 1 - Value','E-mail 2 - Type','E-mail 2 - Value','Phone 1 - Type','Phone 1 - Value','Phone 2 - Type','Phone 2 - Value','Website 1 - Type','Website 1 - Value'
+'''
